b5c9e7714f87cbfc02bcbbfb268d1de76be98f40
Devskiller <owl@devskiller.com>
2022-03-01 16:24:06 +0000

    Update src/main/java/com/devskiller/service/ItemService.java

--- src/main/java/com/devskiller/service/ItemService.java
+++ src/main/java/com/devskiller/service/ItemService.java
@@ -13,7 +13,7 @@ public class ItemService {
     }
 
     public List<String> getTitlesWithAverageRatingLowerThan(Double rating) {
-        
+        itemRepository.gi
         return null;
     }
 
